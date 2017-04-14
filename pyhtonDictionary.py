d9a8953ae22112a74a358ab6e336395b045b1e98
Ramzan Dieze <ramzandieze35@gmail.com>
2017-04-14 08:47:40 +0530

    initial commit

---
+++ pyhtonDictionary.py
@@ -0,0 +1,14 @@
+#def sum_to(x):
+#   return x + sum_to(x-1)
+#print (sum_to(5))
+#def fib(x):
+#  if x == 0 or x == 1:
+#    return 1
+#  else: 
+#    return fib(x-1) + fib(x-2)
+#print(fib(4))
+letters = {"a", "b", "c", "d"}
+if "e" not in letters:
+  print(1)
+else: 
+  print(2)
