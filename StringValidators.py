9709df35eeb14ca6ead53041d68b083807c24b01
Ramzan Dieze <ramzandieze35@gmail.com>
2017-04-14 08:49:48 +0530

    initial commit

---
+++ StringValidators.py
@@ -0,0 +1,26 @@
+s = input()
+
+if(s.isalnum()):
+    print("TRUE")
+else:
+    print("FALSE")
+
+if(s.isalpha()):
+    print("TRUE")
+else:
+    print("FALSE")
+
+if(s.isdigit()):
+    print("TRUE")
+else:
+    print("FALSE")
+
+if(s.islower()):
+    print("TRUE")
+else:
+    print("FALSE")
+
+if(s.isupper()):
+    print("TRUE")
+else:
+    print("FALSE")
